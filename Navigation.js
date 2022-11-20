de25f61cd15b1f80af87fe4110fe525043ef334b
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-20 16:06:03 +0700

    finished LogIn LogOut

--- Navigation.js
+++ Navigation.js
@@ -1,30 +1,55 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
-import GetStart from './src/screen/GetStart'
-import Home from './src/screen/Home'
-import Favourite from './src/screen/Favourite'
-import Detail from './src/screen/Detail'
-
+import GetStart from "./src/screen/GetStart";
+import Home from "./src/screen/Home";
+import Favourite from "./src/screen/Favourite";
+import Detail from "./src/screen/Detail";
+import Login from "./src/screen/Login";
+import Register from "./src/screen/Register";
 
 function StackNavigater() {
-  return (
-    <Stack.Navigator>
-        <Stack.Screen name="GetStart" options={{ headerMode: 'none' }} component={GetStart} />
-        <Stack.Screen name="Home" options={{ headerMode: 'none' }} component={Home} />
-        <Stack.Screen name="Favourite" options={{ headerMode: 'none' }} component={Favourite} />
-        <Stack.Screen name="Detail" options={{ headerMode: 'none' }} component={Detail} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+                name="GetStart"
+                options={{ headerMode: "none" }}
+                component={GetStart}
+            />
+            <Stack.Screen
+                name="Home"
+                options={{ headerMode: "none" }}
+                component={Home}
+            />
+            <Stack.Screen
+                name="Favourite"
+                options={{ headerMode: "none" }}
+                component={Favourite}
+            />
+            <Stack.Screen
+                name="Detail"
+                options={{ headerMode: "none" }}
+                component={Detail}
+            />
+            <Stack.Screen
+                name="Login"
+                options={{ headerMode: "none" }}
+                component={Login}
+            />
+            <Stack.Screen
+                name="Register"
+                options={{ headerMode: "none" }}
+                component={Register}
+            />
+        </Stack.Navigator>
+    );
 }
-const Main = ()=>{
-  return(
-    <NavigationContainer>
-      <StackNavigater>
-
-      </StackNavigater>
-    </NavigationContainer>
-  )
-}
+const Main = () => {
+    return (
+        <NavigationContainer>
+            <StackNavigater></StackNavigater>
+        </NavigationContainer>
+    );
+};
 
 export default Main;
